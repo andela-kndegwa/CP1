1094d53a8e49ae60de6d62653340e8340977b003
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-10-02 01:23:33 +0300

    (Chore) Adds ft-ui branch (Feature) Adds intuitive UI with ui.py file

---
+++ Amity/ui.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import time
+import click
+from colorama import init
+from termcolor import cprint
+from pyfiglet import figlet_format
+
+
+def loading_amity_animation():
+    os.system('clear')
+    print "Loading Amity...\\",
+    syms = ['\\', '|', '/', '-']
+    for _ in range(10):
+        for sym in syms:
+            sys.stdout.write("\b %s" % sym)
+            sys.stdout.flush()
+            time.sleep(0.04)
+    click.secho("DONE!", bold=True)
+
+
+def enter_amity():
+    click.secho('=' * 75, fg='cyan')
+    click.secho('*' * 75, fg='yellow')
+    click.secho('=' * 75, fg='cyan')
+    init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
+    cprint(figlet_format('AMITY', font='roman'),
+           'cyan', attrs=['bold'])
+    click.secho('=' * 75, fg='cyan')
+    click.secho('*' * 75, fg='yellow')
+    click.secho('=' * 75, fg='cyan')
+
+loading_amity_animation()
+enter_amity()
